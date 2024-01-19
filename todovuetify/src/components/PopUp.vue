9b9830b7e7e1e5559569bb30bee3aa8576888b2d
Muskan-kennect <156775104+Muskan-kennect@users.noreply.github.com>
2024-01-19 16:18:07 +0530

    Update PopUp.vue for vuetify functionalities

--- todovuetify/src/components/PopUp.vue
+++ todovuetify/src/components/PopUp.vue
@@ -1,199 +1,204 @@
 <template>
-  <div class="full-container">
+  <v-container fluid class="full-container">
     <!-- First div with an input field and a + button -->
-    <div class="input-container">
-      <input
-        v-model="newTask"
-        placeholder="Create New Task ..."
-        class="custom-input"
-      />
-      <button @click="addTaskToStore" class="custom-button">+</button>
-    </div>
+    <v-row class="input-container">
+      <v-col>
+        <v-text-field
+          v-model="newTask"
+          placeholder="Create New Task ..."
+          class="custom-input"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn @click="addTaskToStore" class="custom-button">+</v-btn>
+      </v-col>
+    </v-row>
 
     <!-- Second div with three labels -->
-    <div class="labels-container">
-      <div class="custom-label default-color">All Tasks: {{ tasks.length }}</div>
-      <div class="custom-label incomplteted-color">
+    <v-row class="labels-container">
+      <v-col class="custom-label default-color">
+        All Tasks: {{ tasks.length }}
+      </v-col>
+      <v-col class="custom-label incomplteted-color">
         Incomplete: {{ incompleteTasksCount }}
-      </div>
-      <div class="custom-label completed-color">
+      </v-col>
+      <v-col class="custom-label completed-color">
         Completed: {{ completedTasksCount }}
-      </div>
-
-      <div class="search-input-container">
-      <input v-model="searchTerm" placeholder="Search Task ..." class="search-input" @input="searchTasks" />
-    </div>
-    </div>
+      </v-col>
+      <!-- <v-col class="search-input-container">
+        <v-text-field
+          v-model="searchTerm"
+          placeholder="Search Task ..."
+          class="search-input"
+          @input="searchTasks"
+        ></v-text-field>
+      </v-col> -->
+    </v-row>
 
     <!-- Third div with an image -->
-    <div class="image-container">
-      <div class="sper-set-task-container">
-        <div
-          v-for="task in incompleteTasks"
-          :key="task.id"
-          class="task-item"
-          @mouseover="showButtons(task.id)"
-          @mouseleave="hideButtons(task.id)"
-        >
-          <div class="task-item-container">
-            <div
-              class="task-checkbox"
-              :class="{ 'completed': task.isCompleted }"
-              @click="toggleTaskCompletion(task.id)"
-            >
-              <img
-                v-if="task.isCompleted"
-                src="https://th.bing.com/th/id/OIP.Cc9rDrBzCE-wwGGyqLdy0AHaIz?w=253&h=215&c=7&r=0&o=5&pid=1.7"
-                alt="Green Tick"
-              />
-            </div>
-            <div class="task-title">
-              <div v-if="editingTaskId === task.id">
-                <input v-model="editedTaskTitle" class="edit-input" />
-                <button class="save-task" @click="saveTask(task.id)">Save</button>
-                <button class="cancel1-task" @click="cancelEdit(task.id)">Cancel</button>
-              </div>
-              <div v-else>
-                {{ task.title }}
-            <div class="edit-task" @click="editTask(task.id)">Edit</div>
-              </div>
-            </div>
-           
-          </div>
-        </div>
-        <div v-for="task in completedTasks" :key="task.id" class="task-item incompletedTask">
-          <div class="task-item-container">
-            <div
-              class="task-checkbox"
-              :class="{ 'completed': task.isCompleted }"
-              @click="toggleTaskCompletion(task.id)"
-            >
-              <img
-                v-if="task.isCompleted"
-                src="https://th.bing.com/th/id/OIP.Cc9rDrBzCE-wwGGyqLdy0AHaIz?w=253&h=215&c=7&r=0&o=5&pid=1.7"
-                alt="Green Tick"
-              />
-            </div>
-            <div class="task-title">{{ task.title }}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <br />
+    <v-row class="image-container">
+      <v-col class="sper-set-task-container">
+        <v-row v-for="task in filteredTasks" :key="task.id" class="task-item">
+          <v-col
+            @mouseover="showButtons(task.id)"
+            @mouseleave="hideButtons(task.id)"
+          >
+            <v-row class="task-item-container">
+              
+             
+              <v-col class="task-checkbox" v-if="editingTaskId === null "  >
+               <v-container v-if="task.isCompleted === 0 " >
+                <v-checkbox :model-value="task.isCompleted" @update:model-value="toggleTaskCompletion(task.id)"></v-checkbox>
+              </v-container> 
+                <v-container v-if="task.isCompleted === 1 " ><img src="https://th.bing.com/th?id=OIP.2SpQvGATzbVDA_jwcVaO7QHaIe&w=233&h=267&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="Green Tick" class="tick-image" /></v-container>
+
+              </v-col>
+
+              <v-col class="task-title">
+                <v-row v-if="editingTaskId === task.id" class="modal-edit-view">
+                  <v-text-field v-model="editedTaskTitle" class="edit-input"></v-text-field>
+                  <v-container class="edit-view-container">
+                  <v-btn @click="saveTask(task.id)" class="save-task">Save</v-btn>
+                  <v-btn @click="cancelEdit(task.id)" class="cancel1-task">Cancel</v-btn>
+                  </v-container>
+                </v-row>
+                <v-row v-else>
+                  {{ task.title }}
+                  <v-btn @click="editTask(task.id)" class="edit-task">Edit</v-btn>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
 
     <!-- Fourth div with a button -->
-    <div class="button-container">
-      <label @click="clearCompletedTasks" class="clear-task-container">Clear Completed Tasks</label>
-    </div>
-  </div>
+    <v-row class="button-container">
+      <v-label @click="clearCompletedTasks" class="clear-task-container"
+        >Clear Completed Tasks</v-label
+      >
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
 import { useTaskStore } from '../store/index';
 
 export default {
-data() {
-  return {
-    newTask: '',
-    editingTaskId: null,
-    editedTaskTitle: '',
-    searchTerm: '', // Add searchTerm data property
-  };
-},
-computed: {
-  tasks() {
-    return useTaskStore().tasks;
+  props: {
+    searchTasks: {
+      type: Function,
+      default: function() {
+        return useTaskStore().tasks;
+      },
+    },
+    searchTerm: {
+      type: String,
+      default: '',
+    },
   },
-  completedTasksCount() {
-    return this.tasks.filter((task) => task.isCompleted).length;
+  data() {
+    return {
+      newTask: '',
+      editingTaskId: null,
+      editedTaskTitle: '',
+    };
   },
-  incompleteTasksCount() {
-    return this.tasks.filter((task) => !task.isCompleted).length;
-  },
-  completedTasks() {
-    return this.getCompletedTasks();
-  },
-  incompleteTasks() {
-    return this.getIncompleteTasks();
-  },
-  // Filtered tasks based on searchTerm
-  filteredTasks() {
-    if (this.searchTerm) {
-      const searchLower = this.searchTerm.toLowerCase();
-      return this.tasks.filter((task) => task.title.toLowerCase().includes(searchLower));
-    } else {
-      return this.tasks;
-    }
-  },
-
-},
-methods: {
-  addTaskToStore() {
-    useTaskStore().addTask(this.newTask);
-    this.newTask = '';
-  },
-  toggleTaskCompletion(taskId) {
-    const task = this.tasks.find((t) => t.id === taskId);
-    if (task) {
-      task.isCompleted = !task.isCompleted;
-    }
-  },
-  clearCompletedTasks() {
-    this.tasks = this.tasks.filter((task) => {
-      if(task.isCompleted){
-          task.isCompleted = !task.isCompleted;
+  computed: {
+    tasks() {
+      return useTaskStore().tasks;
+    },
+    completedTasksCount() {
+      return this.tasks.filter((task) => task.isCompleted).length;
+    },
+    incompleteTasksCount() {
+      return this.tasks.filter((task) => !task.isCompleted).length;
+    },
+    completedTasks() {
+      return this.getCompletedTasks();
+    },
+    incompleteTasks() {
+      return this.getIncompleteTasks();
+    },
+    // Filtered tasks based on searchTerm
+    filteredTasks() {
+      if (this.searchTerm) {
+        const searchLower = this.searchTerm.toLowerCase();
+        return this.tasks.filter((task) =>
+          task.title.toLowerCase().includes(searchLower)
+        );
+      } else {
+        return this.tasks;
       }
-    });
+    },
   },
-  getCompletedTasks() {
+  methods: {
 
+    
+    addTaskToStore() {
+      if (this.newTask.trim() !== '') {
+        useTaskStore().addTask(this.newTask);
+        this.newTask = '';
+      } else {
+        alert('Task title cannot be empty!');
+      }
+    },
+    toggleTaskCompletion(taskId) {
+
+      console.log(taskId);
+      const task = this.tasks.find((t) => t.id === taskId);
+      if (task) {
+        task.isCompleted = !task.isCompleted;
+      }
+    },
+    clearCompletedTasks() {
+      this.tasks = this.tasks.filter((task) => !task.isCompleted);
+    },
+    getCompletedTasks() {
       let temp = this.tasks.filter((task) => task.isCompleted);
       if (this.searchTerm) {
-      const searchLower = this.searchTerm.toLowerCase();
-      return temp.filter((task) => task.title.toLowerCase().includes(searchLower));
-    } else {
-      return temp;
-    }
-  },
-  getIncompleteTasks() {
-    
+        const searchLower = this.searchTerm.toLowerCase();
+        return temp.filter((task) =>
+          task.title.toLowerCase().includes(searchLower)
+        );
+      } else {
+        return temp;
+      }
+    },
+    getIncompleteTasks() {
       let temp = this.tasks.filter((task) => !task.isCompleted);
       if (this.searchTerm) {
-      const searchLower = this.searchTerm.toLowerCase();
-      return temp.filter((task) => task.title.toLowerCase().includes(searchLower));
-    } else {
-      return temp;
-    }
-  },
-  editTask(taskId) {
-    const task = this.tasks.find((t) => t.id === taskId);
-    if (task) {
-      this.editingTaskId = taskId;
-      this.editedTaskTitle = task.title;
-    }
-  },
-  cancelEdit() {
-    this.editingTaskId = null;
-    this.editedTaskTitle = '';
-  },
-  saveTask(taskId) {
-    const task = this.tasks.find((t) => t.id === taskId);
-    if (task) {
-      task.title = this.editedTaskTitle;
+        const searchLower = this.searchTerm.toLowerCase();
+        return temp.filter((task) =>
+          task.title.toLowerCase().includes(searchLower)
+        );
+      } else {
+        return temp;
+      }
+    },
+    editTask(taskId) {
+      const task = this.tasks.find((t) => t.id === taskId);
+      if (task) {
+        this.editingTaskId = taskId;
+        this.editedTaskTitle = task.title;
+      }
+    },
+    cancelEdit() {
       this.editingTaskId = null;
       this.editedTaskTitle = '';
-    }
+    },
+    saveTask(taskId) {
+      const task = this.tasks.find((t) => t.id === taskId);
+      if (task) {
+        task.title = this.editedTaskTitle;
+        this.editingTaskId = null;
+        this.editedTaskTitle = '';
+      }
+    },
+    showButtons() {},
+    hideButtons() {},
   },
-  showButtons() {},
-  hideButtons() {},
-  // Function to search tasks based on searchTerm
-  searchTasks() {
-    // You can add additional logic if needed
-    console.log('Search functionality here');
-   
-  },
-},
 };
 </script>
 
@@ -208,17 +213,29 @@ height: 100%;
 
 .input-container {
 position: relative;
-height: 20%;
+height: 10%;
 display: flex;
 flex-direction: row;
 }
 
 .search-input-container {
-margin-top: -6%;
-margin-left: 10%;
+
+margin-left: 5%;
 }
 
+.checkbox-class{
+  width: 2%;
+  height: 2%;
+}
+
+.edit-view-container{
+  display: flex;
+  flex-direction: row;
+  margin-left: 30%;
+}
 .search-input {
+  height: 30%;
+  margin-top: 2%;
 border-radius: 12px;
 }
 
@@ -226,6 +243,20 @@ border-radius: 12px;
 flex-direction: column;
 width: 100%;
 height: 100%;
+}
+
+.tick-image {
+  width: 20px;
+  height: 20px;
+}
+
+.modal-edit-view{
+  top: 50%;
+  background-color: rgba(174, 160, 160, 0.337);
+  left: 10%;
+  height: 50%;
+  border-radius:20px ;
+  
 }
 
 .task-item {
@@ -255,12 +286,17 @@ background-color: rgb(210, 207, 207);
 }
 
 .task-checkbox {
-width: 20px;
-height: 20px;
+  height: 5px;
+  width: 5px;
 border: 2px solid #333;
 border-radius: 50%;
 margin-right: 8px;
 cursor: pointer;
+}
+
+.completed-checkbox .v-input--selection-controls__input {
+  border-color: #4caf50;
+  background-color: #4caf50;
 }
 
 .task-checkbox img {
@@ -287,7 +323,6 @@ width: 80%; /* Adjust as needed */
 display: none;
 position: absolute;
 right: 0;
-margin-top: -4%;
 padding: 8px;
 cursor: pointer;
 opacity: 0;
@@ -339,17 +374,22 @@ opacity: 1;
 }
 
 .edit-input {
-width: 60%;
+width: 40%;
 padding: 6px;
 border-radius: 12px;
-margin-right: 6px;
+
+margin-top: 4px;
+margin-left: 4 px;
+background-color: whitesmoke;
+color: black;
 }
 
 .custom-input {
-width: 100%;
+width: 96%;
 height: 80%;
 font-size: 24px;
-color: grey;
+position: absolute;
+color: black;
 border-bottom-width: 2px;
 border-top-width: 0px;
 border-left-width: 0px;
@@ -357,22 +397,24 @@ border-right-width: 0px;
 border-radius: 12px;
 border-color: rgb(166, 165, 165);
 font-family: inherit;
-padding-left: 2%;
 margin: 0;
 }
 
 .custom-button {
-background-color: rgba(38, 38, 111, 0.716);
+background-color: rgba(53, 53, 138, 0.716);
 color: white;
+position: relative;
 border-radius: 50%;
+margin-right: 5%;
+margin-top: 3%;
+margin-left: 10%;
 border-color: aliceblue;
 border-bottom-width: 2px;
 border-color: gray;
 font-size: 26px;
 cursor: pointer;
 position: absolute;
-right: 10px;
-top: 50%;
+
 width: 8%;
 height: 60%;
 transform: translateY(-50%);
@@ -380,7 +422,7 @@ transform: translateY(-50%);
 
 .labels-container {
 display: flex;
-height: 15%;
+height: 20%;
 padding: 2%;
 border-width: 8px;
 border-color: black;
@@ -393,11 +435,11 @@ font-size: 12px;
 color: white;
 font-family: inherit;
 font-size: 14px;
-margin-top: -5%;
 margin-left: 2%;
 border-radius: 6px;
 padding-top: 0.5%;
 width: 20%;
+margin: 3%;
 height: 60%;
 box-sizing: border-box;
 }
@@ -416,14 +458,19 @@ background-color: rgb(115, 29, 29);
 
 .clear-task-container {
 cursor: pointer;
+font-size: 28px;
+margin-top: 1%;
 color: rgb(167, 166, 166);
+}
+
+.clear-task-container :hover{
+  color: black;
 }
 
 .image-container {
 display: flex;
 min-height: 70%;
 overflow-y: auto;
-margin-top: -4%;
 border: 3px solid rgb(190, 184, 184);
 align-items: center;
 justify-content: center;
