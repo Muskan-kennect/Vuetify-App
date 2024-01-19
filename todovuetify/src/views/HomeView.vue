637ef77fd80e4feb59ae10554a76382fce30bbc4
Muskan-kennect <156775104+Muskan-kennect@users.noreply.github.com>
2024-01-19 16:18:48 +0530

    Update HomeView for vuetify

--- todovuetify/src/views/HomeView.vue
+++ todovuetify/src/views/HomeView.vue
@@ -1,23 +1,69 @@
 <template>
-  <div class="home">
-    <div class="top-half">
+  <v-container fluid class="home">
+    <v-row class="start-container">
       <!-- Content for the top-half goes here -->
-      <h1 class="app-title">Todo App</h1>
-    </div>
-    
-    <div class="overlay"><PopUp/></div>
-    <div class="bottom-half">
-    </div>
-  </div>
+      <v-col class="text-center">
+        <h1 class="app-title">Todo App</h1>
+      </v-col>
+      <v-col class="search-input-container">
+        <v-text-field
+          v-model="searchTerm"
+          placeholder="Search Task ..."
+          class="search-input"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <!-- Overlay Content -->
+      <v-col class="overlay">
+        <PopUp :search-term="searchTerm" :search-tasks="searchTasks"/>
+      </v-col>
+    </v-row>
+
+    <v-row class="end-container">
+      <!-- Other Content for the bottom-half goes here -->
+      <v-col >
+        <!-- Add your bottom-half content here -->
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import  PopUp from "../components/PopUp.vue"
+import { useTaskStore } from '../store/index';
 export default {
   name: 'HomeView',
   components:{
     PopUp : PopUp
   }
+  ,
+  data() {
+    return {
+      newTask: '',
+      editingTaskId: null,
+      editedTaskTitle: '',
+      searchTerm: '', // Add searchTerm data property
+    };
+  },
+
+  methods: {
+    // updateTasks(tasks,searchTerm) {
+    //   useTaskStore().updateTasks(tasks);
+    // },
+    searchTasks() {
+      if (this.searchTerm) {
+        const searchLower = this.searchTerm.toLowerCase();
+        return useTaskStore().tasks.filter((task) =>
+          task.title.toLowerCase().includes(searchLower)
+        );
+       
+      } else {
+        return this.tasks;
+      }
+    },
+  },
 }
 </script>
 
@@ -25,17 +71,45 @@ export default {
 .home {
   position: relative; /* Set the position of the container to relative */
   height: 98vh;
-  margin: -0.5%;
-  margin-top: -1%;
-   margin-top: -1%;
-   margin-bottom: 0.75%; /* Set the height of the container to 100% of the viewport height */
+  margin-bottom: 0.75%; /* Set the height of the container to 100% of the viewport height */
+}
+
+.start-container{
+  background-color: rgb(52, 132, 213);
+  height: 49vh;
+}
+
+.end-container{
+  background-color: rgb(35, 14, 112);
+  height: 49vh;
+}
+
+.search-input-container {
+
+margin-left: 5%;
+}
+
+.text-center{
+  top: 0px;
+  left: 0px;
+}
+
+.search-input {
+  height: 30%;
+  margin: 2%;
+  width: 50%;
+  background-color: rgba(164, 202, 235, 0.721);
+  color: #000;
+border-radius: 12px;
 }
 
 .app-title {
-  font-family: 'Abhaya Libre', sans-serif;
+  font-family: Abhaya Libre;
   font-weight: bold;
   top:80%;
-  font-size: 24px; /* Adjust the font size as needed */
+  margin-top: 6%;
+  margin-left: 80%;
+  font-size: 30px; /* Adjust the font size as needed */
 }
 
 
@@ -47,10 +121,11 @@ export default {
 
 .overlay {
   position: absolute; /* Set the position of the overlay to absolute */
-  top: 10%;
+  top: 15%;
   width: 60%;
   left: 20%;
   height: 70%;
+  border: 2px solid rgb(156, 154, 154);
   border-radius: 12px;
   background-color: white/* Adjust the color and opacity of the overlay */
 }
